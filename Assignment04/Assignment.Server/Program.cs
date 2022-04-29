2c53a9ba30cdf71a796e0e966e5dd82c0be861d0
zhenlei520 <wangzhenlei520@gmail.com>
2022-04-29 14:59:54 +0800

    chore: Add UseIsolation

--- Assignment04/Assignment.Server/Program.cs
+++ Assignment04/Assignment.Server/Program.cs
@@ -1,3 +1,5 @@
+using Masa.Contrib.Isolation;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEventBus(eventBusBuilder =>
@@ -7,6 +9,7 @@ builder.Services.AddEventBus(eventBusBuilder =>
         dbOptions => dbOptions.UseSqlServer());
 });
 var app = builder.Build();
+// app.UseIsolation(); // todo: 如果未使用EventBus发布事件，则需要使用此方法
 
 app.MapPost("Register", async ([FromServices] IEventBus eventBus, RegisterUserEvent @event)
     => await eventBus.PublishAsync(@event));
